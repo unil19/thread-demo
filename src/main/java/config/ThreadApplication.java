d5872576606d9549f6d9dbc6c7251a73e6c637a5
unil <unil>
2019-01-08 18:17:49 +0800

    添加线程intercept测试

--- src/main/java/config/ThreadApplication.java
+++ src/main/java/config/ThreadApplication.java
@@ -16,15 +16,32 @@ public class ThreadApplication {
 //        System.out.println("main");
 
         //测试stop造成写入一半的脏数据
-        ReadUserRunner readUserRunner = new ReadUserRunner();
-        UpdateUserRunner updateUserRunner = new UpdateUserRunner();
-        Thread readThread = new Thread(readUserRunner
-        );
-        Thread updateThread = new Thread(updateUserRunner);
-        readThread.start();
-        updateThread.start();
-        Thread.sleep(150);
+//        ReadUserRunner readUserRunner = new ReadUserRunner();
+//        UpdateUserRunner updateUserRunner = new UpdateUserRunner();
+//        Thread readThread = new Thread(readUserRunner
+//        );
+//        Thread updateThread = new Thread(updateUserRunner);
+//        readThread.start();
+//        updateThread.start();
+//        Thread.sleep(150);
 //        updateUserRunner.setStopMe();
-        updateThread.stop();
+//        updateThread.stop();
+
+        Thread thread = new Thread(){
+            public void run(){
+                while (true){
+                    if(Thread.currentThread().isInterrupted()){
+                        System.out.println("线程终止");
+                        break;
+                    }
+                    Thread.yield();
+                    System.out.println("线程执行中");
+                }
+            }
+        };
+        thread.start();
+        Thread.sleep(2000);
+        thread.interrupt();
+
     }
 }
